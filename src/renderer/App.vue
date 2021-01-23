329261f091b5d0a9be74e36204fb01feea94d20e
tamasori <oeri.tamas@gmail.com>
2021-01-23 14:25:05 +0100

    add fix for process not found webpack

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'github-bitbucket-monitoring-dashboard',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
